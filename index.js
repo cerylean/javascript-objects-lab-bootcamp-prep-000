0c99e50fee6cb0f4d7c8a9ed0f7badd2a136b234
Lyssa Treibitz <cerylean@gmail.com>
2018-06-18 14:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {return Object.assign (
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object[key]=value; return object}
 
-function deleteFromObjectByKey(object, key) {delete object[key]; return object}
+function deleteFromObjectByKey(object, key) {var newObj = Object.assign({}, object); delete newObj[key]; return newObj}
 
 function destructivelyDeleteFromObjectByKey(object, key) {delete object[key]; return object}
 
