7e9f318e2b8dbc71466212f31684846a9167bd17
Lyssa Treibitz <cerylean@gmail.com>
2018-06-18 14:38:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {onion: '3 cups', garlic:'2 cloves'}
 
-function updateObjectWithKeyAndValue(object, key, value) {Object.assign ({},object, {[key]:value}); return object}
+function updateObjectWithKeyAndValue(object, key, value) {return Object.assign ({},object, {[key]:value})}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {New.object = {[key]:value}; return object}
 
