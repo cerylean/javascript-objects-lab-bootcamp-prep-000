cb3465018db17dc62a837b8efa21b07e70f40cfe
Lyssa Treibitz <cerylean@gmail.com>
2018-06-18 14:51:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function updateObjectWithKeyAndValue(object, key, value) {return Object.assign (
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object[key]=value; return object}
 
+function removeFromPlaylist(playlist, artist) {delete playlist[artist]; return playlist}
 
